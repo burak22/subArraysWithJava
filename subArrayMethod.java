9304b73180bf652637df5693f25d5c70f10c83d3
burak22 <43789237+burak22@users.noreply.github.com>
2022-10-07 23:33:25 +0300

    Update subArrayMethod.java

--- subArrayMethod.java
+++ subArrayMethod.java
@@ -10,7 +10,7 @@ public class array2d {
         toConsole3D(subArrayList(secondArray));
     }
 
-
+    //Prints out the 3D array given to the console
     public static void toConsole3D(int[][][] array3d){
         for (int i = 0; i < array3d.length; i++) {
             System.out.print("{ ");
@@ -30,11 +30,13 @@ public class array2d {
             System.out.print("\n");
         }
     }
+    // Removes and returns a new array created by removing first n indexes of a 1D Array given
     public static int[] removeFirstNIndex(int[] inputArray1D,int n){
         int[] outputArray = Arrays.copyOfRange(inputArray1D, n, inputArray1D.length);
         return outputArray;
 
     }
+    // Returns a 2D Array consisting of subarrays of only the first index of the 1D Array given
     private static int[][] subArrayListOfFirstIndex(int[] inputArray1D){
         int[][] array2d = new int[inputArray1D.length][];
         for (int i = 0; i < array2d.length; i++) {
@@ -46,6 +48,7 @@ public class array2d {
         }
         return array2d;
     }
+    // Takes a 1D method as input finds the subarray list of first index, logs to a 3D output Array, removes an index, does the same until no index left Returns the 3D output index.
     public static int[][][] subArrayList(int[] inputArray1D){
         int[][][] outputArray3d = new int[inputArray1D.length][][];
         for (int i = 0; i < inputArray1D.length; i++) {
